f7ad318404ac9c853129b8f466ba1c45b5a787f6
aavishkarmishra <aavishkarmishra@gmail.com>
2022-05-18 22:50:00 +0530

    Done

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+export const API = 'https://pokeapi.co/api/v2/pokemon/';
+export const MAX_POKEMONS_GEN = [0, 151, 251, 386];
+export const MAX_POKEMONS_GEN_I = 151;
+export const MAX_POKEMONS_GEN_II = 251;
+export const MAX_POKEMONS_GEN_III = 386;
+export const MAX_POKEMONS_GEN_IV = 493;
+export const MAX_POKEMONS = MAX_POKEMONS_GEN_IV;
+export const BASENAME = '';
